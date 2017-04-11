8df57763bb829a3b075099d160414ef792e3b9a4
Acollie <alex@alexcollie.com>
2017-04-11 18:17:00 +0100

    Window + screenshot

--- Main.py
+++ Main.py
@@ -1,6 +1,25 @@
 import os
+from tkinter import Tk, Label, Button
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Py-Lapse")
+
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Screenshot", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Greetings!")
+        os.system("screencapture test.png")
 
 
-
-
-os.system("screencapture test"+$end)
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Py-lapse
 
 Pylapse is a project to create a simple Timelapse recorder for your desktop. (OSX)
+Since all the alternatives are paid here is a free and open source, version.
+
+
+Features
+-Timelapse of desktop
+-variable time
+-Automatically turn the output into a video with variable fps
